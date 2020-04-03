9399dc84f96cbe2301b8b3c5cd198bf5d9802d43
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 17:32:24 +0530

    Update Solution.java

--- Flow Control Statements/6/Solution.java
+++ Flow Control Statements/6/Solution.java
@@ -1,22 +1,4 @@
 import java.util.Scanner;
-
-/**
- * Write a program to accept gender ("Male" or "Female") and age (1-120) 
- * from command line arguments and print the percentage of interest based on the given conditions.
- * Interest == 8.2% 
- * Gender ==> Female
- * Age    ==>1 to 58
- * Interest == 7.6% 
- * Gender ==> Female
- * Age    ==>59 -120
- * Interest == 9.2% 
- * Gender ==> Male
- * Age    ==>1-60
- * Interest == 8.3% 
- * Gender ==> Male
- * Age    ==>61-120
- */
-
 class Solution {
     public static void main(String[] args) {
         
