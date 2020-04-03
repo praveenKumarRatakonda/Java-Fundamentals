6ffd138f0269b5e146cba80450c0902de8fe341a
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 17:23:52 +0530

    Update Solution.java

--- Flow Control Statements/11/Solution.java
+++ Flow Control Statements/11/Solution.java
@@ -1,7 +1,3 @@
-/**
- *  Write a program to print even numbers between 23 and 57,
- *  each number should be printed in a separate row.
- */
 
 class Solution {
     public static void main(String[] args) {
