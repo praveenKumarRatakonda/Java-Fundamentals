a5b5fd973984b0e6c50c88b8b277b241b1c91374
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 17:24:35 +0530

    Update Solution.java

--- Flow Control Statements/12/Solution.java
+++ Flow Control Statements/12/Solution.java
@@ -1,9 +1,4 @@
 import java.util.Scanner;
-
-/**
- * Write a program to check if a given number is prime or not
- */
-
 class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
