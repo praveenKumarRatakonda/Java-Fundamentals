fd9e86a4c88e74a62fdaea0c95916c52f611e951
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 17:18:19 +0530

    Update Solution.java

--- Flow Control Statements/7/Solution.java
+++ Flow Control Statements/7/Solution.java
@@ -1,10 +1,5 @@
 import java.util.Scanner;
 
-/**
- *  Write a program to convert from upper case to lower case and vice versa of an alphabet
- *  and print the old character and new character as shown in example (Ex: a->A, M->m).
- */
-
 class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
