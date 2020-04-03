bb1cef37970b699fad2618cae7ef870e561f60a0
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 17:40:53 +0530

    Update Solution.java

--- Arrays/3/Solution.java
+++ Arrays/3/Solution.java
@@ -1,11 +1,4 @@
 import java.util.Scanner;
-
-/**
- *  Write a program to initialize an integer array with values and check if a given number
- *  is present in the array or not. If the number is not found, it will print -1 else it will
- *  print the index value of the given  number in the array
- */
-
 class Solution {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 7, 8, 9, 0 };
