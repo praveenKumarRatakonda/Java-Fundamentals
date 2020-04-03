342915ae2115ea577867d6477952af10ae1c6177
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 13:45:37 +0530

    Update Solution.java

--- Flow Control Statements/4/Solution.java
+++ Flow Control Statements/4/Solution.java
@@ -1,10 +1,10 @@
-/**
- * Initialize two character variables in a program and display the characters in alphabetical order. 
- * Eg1) if first character is s and second is e
- * O/P: e,s
- * Eg2) if first character is a and second is e
- * O/P:a,e
- */
+
+
+
+ 
+
+ 
+
 
 class Solution {
     public static void main(String[] args) {
