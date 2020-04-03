bc8450275fd5044e0f145c5c4d247d56cb598bfb
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-04-03 17:41:45 +0530

    Update Solution.java

--- Arrays/4/Solution.java
+++ Arrays/4/Solution.java
@@ -1,8 +1,4 @@
-/**
- *  Initialize an integer array with ascii values and print the corresponding character
- *  values in a single row.
- */
-class Solution {
+ class Solution {
     public static void main(String[] args) {
         int ascii[] = {73, 32, 76, 111, 118, 101, 32, 89, 111, 117};
         for (int i = 0; i < ascii.length; i++) {
